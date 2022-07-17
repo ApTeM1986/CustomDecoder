de0eac8b0211bf6fb17b7803e42dfc9f6c7defe8
ApTeM1986 <103114269+ApTeM1986@users.noreply.github.com>
2022-07-17 15:27:05 +0200

    Update CurrentWeatherData.swift

--- CustomDecoder/CurrentWeatherData.swift
+++ CustomDecoder/CurrentWeatherData.swift
@@ -4,5 +4,20 @@
 //
 //  Created by Artem Stetsenko on 17.07.2022.
 //
-
-import Foundation
+struct CurrentWeatherData: Codable{
+    let dt: Int
+    let sunrise: Int
+    let sunset: Int
+    let temp: Double
+    let feels_like: Double //
+    let pressure: Int
+    let humidity: Int
+    let dew_point: Double
+    let uvi: Double
+    let clouds: Int
+    let visibility: Int
+    let wind_speed: Int //
+    let wind_deg: Int //
+    let weather: [CurrentWeatherParams]
+    let rain: Rain   
+}
